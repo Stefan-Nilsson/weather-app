8cc817e87877a8e28bdb7eddbc4a4c25389eb8fd
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2020-01-10 16:15:24 +0100

    fix metric

--- script.js
+++ script.js
@@ -33,33 +33,34 @@ async function getCity() {
 
 
  */
+
 // Application state object.
 
 async function getWeatherBoy() {
     let varInput = document.getElementById("input1");
-    let varInputTwo = document.getElementById("input2")
+    let varInputTwo = document.getElementById("input2");
     let varOutputWeather = document.getElementById("output");
-    let varCity = "http://api.openweathermap.org/data/2.5/forecast?q=" + varInput.value // + varInputTwo + "&mode=xml";
-    let varKey = "&appid=21e65cce6bce035b3e041877c941e391";
-    let varFetch = varCity + varKey;
-    const response = await fetch(varFetch);
+    // let varCity = "http://api.openweathermap.org/data/2.5/forecast?q=" + varInput.value + varInputTwo.value + "&mode=xml"; // +  // + varInputTwo + "&mode=xml";
+    // let theKey = "&appid=21e65cce6bce035b3e041877c941e391";
+    // let varFetch = varCity + theKey + "&units=metric";
+    const response = await fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + varInput.value + ',' + varInputTwo.value + '&appid=21e65cce6bce035b3e041877c941e391'+ '&units=metric');
     const weatherList = await response.json();
     varOutputWeather.innerText = weatherList;
     console.log("hello");
     console.log(weatherList.list);
 
 
-/*
-    const input = {
-        jsonText: weatherList.json,
-        jsonPretty: null,
-        headers: weatherList.headers,
-        activeTab: 0,
-        prettified: false,
-        expandedNodes: new Set(),
-    };
-    console.log(input)
-*/
+    /*
+        const input = {
+            jsonText: weatherList.json,
+            jsonPretty: null,
+            headers: weatherList.headers,
+            activeTab: 0,
+            prettified: false,
+            expandedNodes: new Set(),
+        };
+        console.log(input)
+    */
 
 }
 
