20296dfa0a14efaf653eae08a24bfdcc7273cd3d
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2020-01-13 10:39:18 +0100

    trying out some stuff

--- script.js
+++ script.js
@@ -37,20 +37,53 @@ async function getCity() {
 // Application state object.
 
 async function getWeatherBoy() {
+
     let varInput = document.getElementById("input1");
     let varInputTwo = document.getElementById("input2");
     let varOutputWeather = document.getElementById("output");
-    // let varCity = "http://api.openweathermap.org/data/2.5/forecast?q=" + varInput.value + varInputTwo.value + "&mode=xml"; // +  // + varInputTwo + "&mode=xml";
-    // let theKey = "&appid=21e65cce6bce035b3e041877c941e391";
-    // let varFetch = varCity + theKey + "&units=metric";
+    const current = new Date();
+    const currentDate = current.getDate();
+
     const response = await fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + varInput.value + ',' + varInputTwo.value + '&appid=21e65cce6bce035b3e041877c941e391'+ '&units=metric');
     const weatherList = await response.json();
-    varOutputWeather.innerText = weatherList;
+    varOutputWeather.innerHTML = weatherList.list[0].dt;
     console.log("hello");
     console.log(weatherList.list);
 
 
+
+    let i;
+
+        let dateUnics = weatherList.list[2].dt;
+        var date = new Date(dateUnics * 1000);
+        var hours = date.getHours();
+        let dayUnics = date.getDate();
+        console.log(dateUnics);
+        console.log(currentDate);
+        console.log(hours);
+        for (i=0; i > weatherList.list.length;i++)
+            if (hours = 00)
+        if (dateUnics == currentDate){
+
+
+        }
+
+        else if (dateUnics == currentDate + 1) {
+
+        }
+
+        else if
+  //  for (i = 0; i > weatherList.list[i].length; i++) {
+
+
     /*
+
+     // let varCity = "http://api.openweathermap.org/data/2.5/forecast?q=" + varInput.value + varInputTwo.value + "&mode=xml"; // +  // + varInputTwo + "&mode=xml";
+    // let theKey = "&appid=21e65cce6bce035b3e041877c941e391";
+    // let varFetch = varCity + theKey + "&units=metric";
+
+
+
         const input = {
             jsonText: weatherList.json,
             jsonPretty: null,
